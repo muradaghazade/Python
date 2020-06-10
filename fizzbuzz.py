2a0fb42938400bfb73937583c49f9eeb49e6b9ed
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-06-10 17:02:20 +0400

    Add files via upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,11 @@
 list  = []
 
 for i in range(1, 101):
-    if i%3 == 8:
+    if i%3 ==0 :
         if i%5 == 0:
             list.append("fizzbuzz")
-    elif i%3==0:
-        list.append("fizz")
-    elif i%5== 0:
+            else:
+                list.append("fizz")
         list.append("buzz")
     else:
         list.append("nothing")
