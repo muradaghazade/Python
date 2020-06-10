6aebf0cd8896e9c7583ad992ecc32770da192a1e
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-06-10 16:03:35 +0400

    Add files via upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i%3==0:
         print (i, "fuzz")
-    elif i%5== 0:
+    if i%5== 0:
         print(i, "buzz")
-    elif i%15==0:
+    if i%15==0:
         print(i, "fuzzbuzz")
     else:
         print(i, "nothing")
