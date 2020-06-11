8d8dfabf65da65f93168782f69d1a07896672423
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-06-11 21:23:16 +0400

    Add files via upload

--- r-s-p.py
+++ r-s-p.py
@@ -2,20 +2,37 @@ import random
 
 list = ['Rock', 'Scissors', 'Paper']
 
+games = 0
+human = 0
+comp = 0
+tie = 0
+
 comp_choice = random.choice(list)
 
 while True:
     a = input("Type your choice:\t")
-    b = a.capitalize()
+    if a != "q":
+        b = a.capitalize()
+        games+=1
 
-    if comp_choice == "Scissors" and b == "Rock":
-        print("Human wins")
-    elif comp_choice == "Paper" and b == "Scissors":
-        print("Human wins")
-    elif comp_choice == "Rock" and b == "Paper":
-        print("Human wins")  
-    elif comp_choice == b:
-        print("Tie")
+        if comp_choice == "Scissors" and b == "Rock":
+            human+=1
+            print(f' Human choice: {b}\n Computer choice: {comp_choice}\n Human wins!\n Games played: {games} Human: {human} Computer: {comp} Tie: {tie}')
+        elif comp_choice == "Paper" and b == "Scissors":
+            human+=1
+            print(f' Human choice: {b}\n Computer choice: {comp_choice}\n Human wins!\n Games played: {games} Human: {human} Computer: {comp} Tie: {tie}')
+        elif comp_choice == "Rock" and b == "Paper":
+            human+=1
+            print(f' Human choice: {b}\n Computer choice: {comp_choice}\n Human wins!\n Games played: {games} Human: {human} Computer: {comp} Tie: {tie}') 
+        elif comp_choice == b:
+            tie+=1
+            print(f' Human choice: {b}\n Computer choice: {comp_choice}\n Tie!\n Games played: {games} Human: {human} Computer: {comp} Tie: {tie}')
+        elif b != "Rock" and b != "Scissors" and b != "Paper":
+            print("Do not cheat!")
+        else:
+            comp+=1
+            print(f' H2uman choice: {b}\n Computer choice: {comp_choice}\n Computer wins!\n Games played: {games} Human: {human} Computer: {comp} Tie: {tie}')
+            
     else:
-        print("Computer wins")  
-    break
+        print("Bye babe")
+        break
