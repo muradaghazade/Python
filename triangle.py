8c8fbbe832f6da4084ddd16599c314e7e91a018d
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-06-17 00:37:35 +0400

    Add files via upload

--- triangle.py
+++ triangle.py
@@ -1,5 +1,9 @@
 a = int(input("Enter the lenght:\t"))
 b = input("Enter symbol:\t")
 
-for i in range(1,a+1):
-    print (b * i)
+def triangle_maker(x, y):
+    for i in range(1,x+1):
+        print(b * i)
+
+
+triangle_maker(a, b)
